fdcdc83defffe45178284c1fad1fb5310328a60b
heroku121 <130366513+heroku121@users.noreply.github.com>
2023-04-10 19:47:29 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,276 @@
+ <?php
+echo '
+<!Doctype html>
+<html>
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+
+<head>
+    <meta charset="UTF-8">
+    <title>Glucotrust Official</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="robots" content="noindex">
+    <link rel="icon" type="image/x-icon" href="images/favicon.png">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+    <div class="head">
+        <div class="containers-gtrt13 flx-gtrt13 jus alig-cen-gtrt13 fl-dr">
+            <a href="https://7b08blil0732fo71wlfj4d-ovs.hop.clickbank.net/?cbpage=dtc&tid=khikhi">
+                <h1 class="logo">Glucotrust</h1>
+            </a>
+            <a href="https://7b08blil0732fo71wlfj4d-ovs.hop.clickbank.net/?cbpage=dtc&tid=khikhi">
+                <div class="site-btn-gtrt13">Order Now Limited Time Offer !!!</div>
+            </a>
+        </div>
+    </div>
+    <div class="hero-gtrt13 ptb">
+        <div class="containers-gtrt13">
+            <div class="hero-gtrt13-bott flx-gtrt13 gp alig-cen-gtrt13 fl-dr">
+                <div class="hero-gtrt13-img"> <img src="images/pr.png" width="400"></div>
+                <div class="hero-gtrt13-inf">
+                    <h3>GlucoTrust Simple Way To Support Your Blood Sugar Levels and Health Weigth-Loss</h3>
+                    <ul class="hero-gtrt13-ben-gtrt13">
+                        <li>Helps Promote Healthy Blood Sugar Levels</li>
+                        <li>Promotes Healthy Metabolism</li>
+                        <li>Supports Healthy Blood Pressure</li>
+                        <li>Helps You Lose Weight</li>
+                        <li>Helps Reduce Food Cravings</li>
+                    </ul>
+                    <a href="https://7b08blil0732fo71wlfj4d-ovs.hop.clickbank.net/?cbpage=dtc&tid=khikhi">
+                        <div class="bts-gtrt13">Order Now</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="what ptb">
+        <div class="containers-gtrt13">
+            <div class="dfdf">
+                <h1 class="txt-cen-gtrt13 title">What is GlucoTrust™?</h1>
+                <p> GlucoTrust is a nutritional supplement in item form that contains the most essential nutrients and powerful antioxidants.</p>
+
+                <p>The unique concentrated formula of this natural supplement detoxifies your body, promotes glucose metabolism, and helps you maintain healthy glucose levels. With a perfect blend of natural ingredients, GlucoTrust optimizes blood health,
+                    controls diabetic symptoms, restores vitality, and improves your overall health and fitness.</p>
+
+                <p>The proprietary blend of powerful antioxidants eliminates unwanted fat and optimizes blood sugar levels in your body. It is a scientifically designed herbal and natural dietary supplement that combats fatigue and increases your energy
+                    levels.
+                </p>
+
+                <p>Their FDA-approved and GMP-certified facility in the USA meets the best standards in the industry. they always make sure that each item is made in a clean and hygienic environment with a clear focus on quality and precision.</p>
+
+                <p>GlucoTrust diet supplements do not contain any genetically modified organisms (GMO). Safety is their number one priority. You don\'t need to worry about any toxic substances or dangerous stimulants when taking this non-addictive supplement.</p>
+            </div>
+        </div>
+    </div>
+    <div class="ben-gtrt13 ptb " style="background: #fafafa">
+        <div class="containers-gtrt13">
+            <div class="txt-cen-gtrt13">
+                <h1 class="title">
+                    Why Choose GlucoTrust™ Supplement
+                </h1>
+                <p>Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
+            </div>
+            <div class="feat-gtrt13ures">
+                <div class="feat-gtrt13ure feat-gtrt13ure-one">
+                    <h2 class="feat-gtrt13ure__title">Made In The USA</h2>
+                    <p class="feat-gtrt13ure__desc">GlucoTrust supplement is manufactured on US soil.</p>
+                    <img class="feat-gtrt13ure__img" src="images/b1.jpg" alt="" width="100">
+                </div>
+                <div class="wrapper">
+                    <div class="feat-gtrt13ure feat-gtrt13ure-two">
+                        <h2 class="feat-gtrt13ure__title">100% All Natural</h2>
+                        <p class="feat-gtrt13ure__desc">All ingredients are pure, natural, and carefully sourced.</p>
+                        <img class="feat-gtrt13ure__img" src="images/b2.png" alt="" width="100">
+                    </div>
+                    <div class="feat-gtrt13ure feat-gtrt13ure-three">
+                        <h2 class="feat-gtrt13ure__title">FDA Approved Facility</h2>
+                        <p class="feat-gtrt13ure__desc">GlucoTrust is manufactured according to the latest standards.</p>
+                        <img class="feat-gtrt13ure__img" src="images/b3.png" alt="" width="100">
+                    </div>
+                </div>
+                <div class="feat-gtrt13ure feat-gtrt13ure-four">
+                    <h2 class="feat-gtrt13ure__title">Satisfaction Guaranteed</h2>
+                    <p class="feat-gtrt13ure__desc">There is a 6-month full refund guarantee from the manufacturer.</p>
+                    <img class="feat-gtrt13ure__img" src="images/b4.png" alt="" width="100">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="cons-gtrt13 ptb">
+        <div class="containers-gtrt13">
+            <div class="dfdf">
+                <h2>And You Can Order With Complete Confidence, Knowing This Is A Risk-Free Offer...</h2>
+                <p> Because when you order any amount of GlucoTrust today—one bottle, three bottles or six—you\'re covered by a 100% money back guarantee good for 180 days. That\'s six full months.And this guarantee is "no questions asked."This means that if
+                    you decide to request a refund, you don\'t need to explain why. It doesn\'t even matter if you never get around to trying it. Your payment is simply returned to you.</p>
+
+                <p>This means that if you decide to request a refund, you don\'t need to explain why. It doesn\'t even matter if you never get around to trying it. Your payment is simply returned to you.And in addition, by ordering a 180 or 90-day supply of
+                    GlucoTrust you\'ll also receive a special bonus package.These are digital bonuses you can start using in just moments. They\'ll help you get started on the right track even before your order of GlucoTrust arrives.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="section-gtrt13-services ptb">
+        <div class="containers-gtrt13">
+            <div class="header-section-gtrt13">
+                <h1 class="title">GlucoTrust Key Natural Ingredients</h1>
+            </div>
+
+            <div class="ing  gp fl-dr">
+
+                <div class="ing-cen-gtrt13">
+                    <div class="single-gtrt13-service">
+                        <div class="content">
+                            <span class="icon">
+                <img src="images/ing.png">
+              </span>
+                            <h3 class="title">Gymnema sylvestra</h3>
+                            <p class="description">First, GlucoTrust contains Gymnema sylvestra, which comes from a leafy vine. It\'s been used for hundreds of years in India for medicinal purposes.</p>
+
+                        </div>
+                        <span class="circle-before"></span>
+                    </div>
+                </div>
+
+                <div class="ing-cen-gtrt13">
+                    <div class="single-gtrt13-service">
+                        <div class="content">
+                            <span class="icon">
+                 <img src="images/ing.png">
+              </span>
+                            <h3 class="title">Licorice</h3>
+                            <p class="description">Licorice root is one of the world\'s oldest herbal remedies, used for thousands of years in traditional Chineses. It\'s also known as a weight loss aid, because it helps to control your appetite while being rich in flavonoids,
+                                which are said to prevent obesity.</p>
+
+                        </div>
+                        <span class="circle-before"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="ing gp fl-dr">
+                <div class="ing-cen-gtrt13">
+                    <div class="single-gtrt13-service">
+                        <div class="content">
+                            <span class="icon">
+                 <img src="images/ing.png">
+              </span>
+                            <h3 class="title">Cinnamon</h3>
+                            <p class="description">It also promotes healthy blood pressure, assists with digestion, and has anti-viral, anti-bacterial properties.</p>
+
+                        </div>
+                        <span class="circle-before"></span>
+                    </div>
+                </div>
+
+                <div class="ing-cen-gtrt13">
+                    <div class="single-gtrt13-service">
+                        <div class="content">
+                            <span class="icon">
+                 <img src="images/ing.png">
+              </span>
+                            <h3 class="title">Biotin</h3>
+                            <p class="description">Next there is biotin, which gets its name from the Greek word "biotos," which means "life." The biotin you get in every GlucoTrust capsule also promotes healthy hair and skin while supporting your liver, eyesight and nervous
+                                system.
+                            </p>
+
+                        </div>
+                        <span class="circle-before"></span>
+                    </div>
+                </div>
+
+            </div>
+            <div class="ing  gp fl-dr">
+                <div class="ing-cen-gtrt13">
+                    <div class="single-gtrt13-service">
+                        <div class="content">
+                            <span class="icon">
+                <img src="images/ing.png">
+              </span>
+                            <h3 class="title">Chromium</h3>
+                            <p class="description">GlucoTrust also contains chromium. This ingredient supports your metabolism and helps it to work faster on burning fat.</p>
+
+                        </div>
+                        <span class="circle-before"></span>
+                    </div>
+                </div>
+                <div class="ing-cen-gtrt13">
+                    <div class="single-gtrt13-service">
+                        <div class="content">
+                            <span class="icon">
+                <img src="images/ing.png">
+              </span>
+                            <h3 class="title">Zinc</h3>
+                            <p class="description">Before finalizing the GlucoTrust formula they also added zinc, which can stimulate insulin production in the pancreas.</p>
+
+                        </div>
+                        <span class="circle-before"></span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="bonus ptb">
+        <div class="containers-gtrt13">
+            <div class="bonus-info">
+                <div class="dfdf">
+                    <h1 class="title txt-cen-gtrt13">3 Free Bonuses for Buyers Now.</h1>
+
+                    <p><span><img src="images/checked1.png" style="width:24px; height:24px;"></span> &nbsp; <b>Digital Bonus #1 is "100 Great Tasting, Fat Burning Green Smoothie Recipes.</b> Here you\'ll discover how to whip up mouth-watering, nutrition-packed
+                        smoothies. You get a whopping 100 smoothie recipes using ingredients you can find at your local grocery store.</p>
+
+                    <p><span><img src="images/checked1.png" style="width:24px; height:24px;"></span> &nbsp; <b>Digital Bonus #2 is "The Ultimate Guide To Superfoods."</b> Processed foods can lead to toxic buildup and weight gain, while superfoods support
+                        your body in every way. This report reveals common foods and snacks that have terrific health ben-gtrt13efits.</p>
+
+                    <p><span><img src="images/checked1.png" style="width:24px; height:24px;"></span> &nbsp;<b>Digital Bonus #3 is "The 3-Day Liver Cleanse Breakthrough"</b> Your liver acts a filter which absorbs and processes the food you take in. You may
+                        have toxins built up in this organ that have been affecting your energy, mood, immune system, and efforts to lose weight. This bonus shows you how to safely and naturally cleanse your liver in just three days.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="price ptb">
+        <div class="containers-gtrt13 ">
+            <h1 class="title txt-cen-gtrt13 mkt">Order GlucoTrust ™ Today - You Save $780 and Get 3 Bonuses Free!</h1>
+            <div class="price-info">
+                <div class="prc-sc flx-gtrt13 gp fl-dr" style="margin:30px 0px;">
+                    <div class="prc-1">
+                        <a href="https://7b08blil0732fo71wlfj4d-ovs.hop.clickbank.net/?cbpage=dtc&tid=khikhi"><img src="images/1.webp" class="prc-se"></a>
+                    </div>
+                    <div class="prc-1">
+                        <a href="https://7b08blil0732fo71wlfj4d-ovs.hop.clickbank.net/?cbpage=dtc&tid=khikhi"><img src="images/2.png" class="prc-se"></a>
+                    </div>
+                    <div class="prc-1">
+                        <a href="https://7b08blil0732fo71wlfj4d-ovs.hop.clickbank.net/?cbpage=dtc&tid=khikhi"><img src="images/3.png" class="prc-se"></a>
+                    </div>
+                </div>
+
+                <a href="https://7b08blil0732fo71wlfj4d-ovs.hop.clickbank.net/?cbpage=dtc&tid=khikhi">
+                    <div class="bts-gtrt13 pt-btn">Order Now</div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <footer style="background: #345085; color: #fff;">
+        <div class="containers-gtrt13 ptb">
+            <div class="footer-info txt-cen-gtrt13">
+                <div class="foot-links ">
+                    <a href="https://getglucotrust.com/privacy">Privacy Policy</a> | <a href="https://getglucotrust.com/terms">Terms</a> | <a href="https://getglucotrust.com/contact">Contact Us</a>
+                </div>
+                <small>Copyright © Glucotrust 2023. All Rights Reserved</small>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
+';
+?>
